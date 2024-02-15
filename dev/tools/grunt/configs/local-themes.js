3b81ebf3794cfe685cb8f056768b4de230fc8fd2
M1seryy <62029606+M1seryy@users.noreply.github.com>
2024-02-15 11:04:57 +0200

    123

---
+++ dev/tools/grunt/configs/local-themes.js
@@ -0,0 +1,9 @@
+"use strict"; module.exports = {
+  theme: {
+    area: 'frontend',
+    name: 'viktor/theme',
+    locale: 'en_US',
+    files: [ 'css/styles-m', 'css/styles-l' ],
+    dsl: 'less'
+  }
+}
